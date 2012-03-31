ccf0f04a09ac623e287f3df06645d32b7923677f
acerix <dylan@psilly.com>
2012-03-31 14:04:05 -0400

    code formatting

--- conf.php
+++ conf.php
@@ -3,21 +3,20 @@
 $scraper_conf = [];
 
 // Location of scrape file to download
-$scraper_conf['uri']	=	'compress.bzip2://http://publicbt.com/all.txt.bz2';
+$scraper_conf['uri']		=	'compress.bzip2://http://publicbt.com/all.txt.bz2';
 
 // Database update method: INSERT, UPDATE, or DUMMY (for no updates)
-$scraper_conf['mode']	=	'DUMMY';
-$scraper_conf['mode']	=	'INSERT';
+$scraper_conf['mode']		=	'DUMMY';
 
 // Database config (see http://php.net/pdo.construct )
-$scraper_conf['db_dsn']	=	'pgsql:dbname=MYDBNAME;host=localhost';
+$scraper_conf['db_dsn']		=	'pgsql:dbname=MYDBNAME;host=localhost';
 $scraper_conf['db_user']	=	'MYDBUSER';
 $scraper_conf['db_pass']	=	'MYDBPASS';
 
 // Verbosity
 // 0 = no output, except errors
 // 9 = full output
-$scraper_conf['v']		=	2;
+$scraper_conf['v']			=	2;
 
 
 // Version name/number
